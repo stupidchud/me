8f5a0d5515825461ed9c5b63646610e0002947cc
e <208794263+6ae95ca5f809@users.noreply.github.com>
2026-04-17 20:19:59 +0300

    bla

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="https://github.com/ravegirl.png" />
+	<link rel="icon" href="https://github.com/6ae95ca5f809.png" />
 	<link rel="stylesheet" href="/css/page.css" />
 	<link rel="stylesheet" href="/css/modal.css" />
 </svelte:head>
